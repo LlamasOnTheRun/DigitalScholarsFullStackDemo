fca112580b379be10edb5076d9bda57d91bde2a5
ProfessorGundar <RyanDL1@hotmail.com>
2023-06-28 18:54:13 -0500

    Found a way to have global state

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import LandingPage from './components/Landing';
-import CreateAccount from './components/CreateAccount';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './components/Login';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-      <Routes>
-        <Route index element={<LandingPage isLogin={false} />} />
-        <Route path="create/account" element={<CreateAccount />} />
-        <Route path="login" element={<Login />} />
-      </Routes>
+      <App />
     </BrowserRouter>
 );
 
